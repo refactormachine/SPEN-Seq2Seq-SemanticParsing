27c7a57c15770ebd861d1cca2f12ecdc136cbee3
veredz <verose@live.com>
2018-08-19 09:10:57 +0300

    add parameter to script to be able to run on GPU

--- run_tangrams.sh
+++ run_tangrams.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
 # Run this script from the root of the project
 BASE_DIR=$(cd $(dirname $0);pwd -P)
-export STRONGSUP_DIR=$BASE_DIR/data
+export STRONGSUP_DIR=${BASE_DIR}/data
+export CUDA_VISIBLE_DEVICES=$1
 echo "=================== Start running script ==================="
-PYTHONPATH=$BASE_DIR:$BASE_DIR/third-party/gtd/ python $BASE_DIR/scripts/main.py $BASE_DIR/configs/rlong/best-tangrams.txt
+echo "using cpu $1"
+PYTHONPATH=${BASE_DIR}:${BASE_DIR}/third-party/gtd/ python ${BASE_DIR}/scripts/main.py ${BASE_DIR}/configs/rlong/best-tangrams.txt
 echo "=================== Finish running script ==================="
