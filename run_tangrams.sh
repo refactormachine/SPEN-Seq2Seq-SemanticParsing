b4a5cabed9b46d00af66fe51e7dcabb6673bd7c1
veredz <verose@live.com>
2018-08-21 18:00:25 +0300

    use tensorflow with gpu

--- run_tangrams.sh
+++ run_tangrams.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Run this script from the root of the project
 BASE_DIR=$(cd $(dirname $0);pwd -P)
+export MKL_THREADING_LAYER="GNU"
+export LD_LIBRARY_PATH="/usr/local/lib/cuda-8.0/lib64/:/usr/local/lib/cudnn-8.0-v7/lib64/"
 export STRONGSUP_DIR=${BASE_DIR}/data
 export CUDA_VISIBLE_DEVICES=$1
 echo "=================== Start running script ==================="
